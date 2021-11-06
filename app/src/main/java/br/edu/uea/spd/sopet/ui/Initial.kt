05a1e4ae0744d6f9fac5265885e19abf2c61b15e
Dayvson Silva <userddssilva@gmail.com>
2021-11-06 09:48:45 -0400

    feat(): save point

---
+++ app/src/main/java/br/edu/uea/spd/sopet/ui/Initial.kt
@@ -0,0 +1,12 @@
+package br.edu.uea.spd.sopet.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.uea.spd.sopet.R
+
+class Initial : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_initial)
+    }
+}
---
+++ app/src/main/java/br/edu/uea/spd/sopet/ui/Login.kt
@@ -0,0 +1,12 @@
+package br.edu.uea.spd.sopet.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.uea.spd.sopet.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
